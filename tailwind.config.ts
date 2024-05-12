6f769cac861e228a2bbb1de8931d7167087bee7e
Min Kuan <betsonavietnam@gmail.com>
2024-05-12 17:00:07 +0700

    integrate system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ['--font-mulish', "sans-serif"],
+        poppins:['var(--font-poppins)',"sans-serif"],
+        k2d:['var(--font-k2d)',"sans-serif"],
+        montserrat:['var(--font-montserrat)',"sans-serif"],
+        nunito:['var(--font-nunito)',"sans-serif"]
+        // Add more custom font families as needed
+      },
     },
   },
   plugins: [],
