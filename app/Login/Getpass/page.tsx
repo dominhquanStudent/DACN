a1441f7e3db350a832f81ee2a1dc1036df6e443c
yamakunson <92131933+yamakunson@users.noreply.github.com>
2024-05-16 21:21:08 +0700

    16/5

---
+++ app/Login/Getpass/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+function Page() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>This is the user profile</p>
+        </div>
+    )
+}
+
+export default Page
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import React, { useEffect } from 'react';
 const RootPage = () => {
   const router = useRouter();
   useEffect(() => {
-    router.replace('/Main');
+    router.replace('/Login');
   }, []);
 
   return <></>;
