2b18a5bf6205a88d5234e8b26a26863730775859
yamakunson <92131933+yamakunson@users.noreply.github.com>
2024-05-21 17:38:52 +0700

    21/5

--- app/Component/ProfileNav/ProfileNav.tsx
+++ app/Component/ProfileNav/ProfileNav.tsx
@@ -1,40 +1,42 @@
-import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 export default function ProfileNav() {
-    const router = useRouter();
 
-    return(
-        <div className="flex flex-col pl-12 mt-12 w-4/12 font-nunito ">
-        {/* Left */}
-        <div className="flex items-center w-full mb-4">
-            <img src="/img/Avatar_vinh.png" alt='Avatar' className="w-12 h-12 rounded-full mr-4" />
-            <div className="text-xl font-bold">Nguyễn Quang Vinh</div>
-        </div>
-        <div className="flex flex-col font-bold w-full text-lg">
-            <div className="flex flex-col w-full">
-                <div className="flex items-center">
-                    <img src="/img/person.svg" alt='Person' className="w-6 h-6 mr-2" />
-                    Tài khoản
-                </div>
-                <div className="ml-8 font-normal">
-                    <div className="my-2">Hồ sơ</div>
-                    <div className="my-2">Địa chỉ</div>
-                    <div className="my-2">Đổi mật khẩu</div>
-                </div>
-            </div>
-            <div className="flex items-center mb-4">
-                <img src="/img/order.svg" alt='Order' className="w-6 h-6 mr-2" />
-                Lịch sử mua hàng
-            </div>
-            <div className="flex items-center mb-4">
-                <img src="/img/notification.svg" alt='Notification' className="w-6 h-6 mr-2" />
-                Cài đặt thông báo
-            </div>
+    return(<div className="flex flex-col pl-12 mt-12 w-4/12 font-nunito ">
+    {/* Left */}
+    <div className="flex items-center w-full mb-4">
+        <img src="/img/Avatar_vinh.png" alt='Avatar' className="w-12 h-12 rounded-full mr-4" />
+        <div className="text-xl font-bold">Nguyễn Quang Vinh</div>
+    </div>
+    <div className="flex flex-col font-bold w-full text-lg">
+        <div className="flex flex-col w-full">
             <div className="flex items-center">
-                <img src="/img/logout.svg" alt='Logout' className="w-6 h-6 mr-2" />
-                Đăng xuất
+                <img src="/img/person.svg" alt='Person' className="w-6 h-6 mr-2" />
+                Tài khoản
             </div>
+            <div className="ml-8 font-normal">
+                <div className={`my-2 ${usePathname() == "/Profile" ? "text-[#EDB24E] font-bold" : ""}`}>
+                    <Link href="/Profile">Hồ sơ </Link></div>
+                <div className={`my-2 ${usePathname() == "/Profile/Address" ? "text-[#EDB24E] font-bold" : ""}`}>
+                    <Link href="/Profile/Address">Địa chỉ </Link></div>
+                <div className={`my-2 ${usePathname() == "/Profile/ChangePass" ? "text-[#EDB24E] font-bold" : ""}`}>
+                    <Link href="/Profile/ChangePass">Đổi mật khẩu</Link></div>
+            </div>
+        </div>
+        <div className="flex items-center mb-4 ">
+            <img src="/img/order.svg" alt='Order' className={` w-6 h-6 mr-2`} />
+            <Link href="/Profile/OrderHistory" className={`${usePathname() == "/Profile/OrderHistory" ? "text-[#EDB24E] font-bold" : ""}`}> Lịch sử đơn hàng</Link>
+        </div>
+        <div className="flex items-center mb-4">
+            <img src="/img/notification.svg" alt='Notification' className={` w-6 h-6 mr-2 ${usePathname() == "/Profile/Notification" ? "text-[#EDB24E] font-bold" : ""}`} />
+            <Link href="/Profile/NotifySetting"className={`${usePathname() == "/Profile/NotifySetting" ? "text-[#EDB24E] font-bold" : ""}`}>Cài đặt thông báo</Link>
+        </div>
+        <div className="flex items-center">
+            <img src="/img/logout.svg" alt='Logout' className={` w-6 h-6 mr-2`} />
+            <Link href="/Profile/Logout">Đăng xuất</Link>
         </div>
     </div>
+</div>
     )
         
     
--- app/Profile/ChangePass/page.tsx
+++ app/Profile/ChangePass/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-import Header from "../../Component/Header/Header";
-import Footer from '../../Component/Footer/Footer';
+import Header from "@/app/Component/Header/Header";
+import Footer from '@/app/Component/Footer/Footer';
+import ProfileNav from "@/app/Component/ProfileNav/ProfileNav";
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 function Page() {
-    const [page, setPage] = useState("changepass")
     const [passwords, setPasswords] = useState(["", "", ""]);
     const [hidepass, setHidepass] = useState([false, false, false]);
 
@@ -32,49 +32,15 @@ function Page() {
         <div className="flex flex-col w-full ">
             <Header />
             <div className="flex bg-[#DFF3FF] w-full ">
-                <div className="flex flex-col pl-12 mt-12 w-4/12 font-nunito ">
-                    {/* Left */}
-                    <div className="flex items-center w-full mb-4">
-                        <img src="/img/Avatar_vinh.png" alt='Avatar' className="w-12 h-12 rounded-full mr-4" />
-                        <div className="text-xl font-bold">Nguyễn Quang Vinh</div>
-                    </div>
-                    <div className="flex flex-col font-bold w-full text-lg">
-                        <div className="flex flex-col w-full">
-                            <div className="flex items-center">
-                                <img src="/img/person.svg" alt='Person' className="w-6 h-6 mr-2" />
-                                Tài khoản
-                            </div>
-                            <div className="ml-8 font-normal">
-                                <div className={`my-2 ${page == "profile" ? "text-[#EDB24E] font-bold" : ""}`}>
-                                    <Link href="/Profile">Hồ sơ </Link></div>
-                                <div className={`my-2 ${page == "address" ? "text-[#EDB24E] font-bold" : ""}`}>
-                                    <Link href="/Profile/Address">Địa chỉ </Link></div>
-                                <div className={`my-2 ${page == "changepass" ? "text-[#EDB24E] font-bold" : ""}`}>
-                                    <Link href="/Profile/ChangePass">Đổi mật khẩu</Link></div>
-                            </div>
-                        </div>
-                        <div className="flex items-center mb-4">
-                            <img src="/img/order.svg" alt='Order' className={` w-6 h-6 mr-2 ${page == "order" ? "text-[#EDB24E] font-bold" : ""}`} />
-                            <Link href="/Profile/OrderHistory">Lịch sử đơn hàng</Link>
-                        </div>
-                        <div className="flex items-center mb-4">
-                            <img src="/img/notification.svg" alt='Notification' className={` w-6 h-6 mr-2 ${page == "notify" ? "text-[#EDB24E] font-bold" : ""}`} />
-                            <Link href="/Profile/NotifySetting">Cài đặt thông báo</Link>
-                        </div>
-                        <div className="flex items-center">
-                            <img src="/img/logout.svg" alt='Logout' className={` w-6 h-6 mr-2 ${page == "logout" ? "text-[#EDB24E] font-bold" : ""}`} />
-                            <Link href="/Profile/Logout">Đăng xuất</Link>
-                        </div>
-                    </div>
-                </div>
+                <ProfileNav />
                 {/* Right */}
                 <div className="flex flex-col w-full bg-white py-4 px-12 my-8 mr-12 rounded-lg shadow-lg">
                     <div className="mb-2 font-nunito font-bold text-2xl">Đổi mật khẩu</div>
                     <div className='border border-[#C5C5CF] w-full mr-5 mx-4'></div>
                     <div className="flex flex-col py-2 mr-8">
                         {passwordFields.map((field) => (
-                            <div className="flex items-center my-2" key={field.label}>
-                                <div className="font-nunito w-1/4">{field.label}</div>
+                            <div className="flex items-center my-2 space-y-4" key={field.label}>
+                                <div className="font-nunito w-1/4 ">{field.label}</div>
                                 <div className="relative flex items-center font-nunito w-1/2 text-lg">
                                     <input
                                         type={hidepass[field.hidepassIndex] ? "text" : "password"}
