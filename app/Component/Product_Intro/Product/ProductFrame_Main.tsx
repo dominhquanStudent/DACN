3db50cac2c2670bf12d4942e86732df436f96419
Do Minh Quan <quan.dobestsona@hcmut.edu.vn>
2024-09-03 20:58:33 +0700

    Update Frame for dynamic cart

--- app/Component/Product_Intro/Product/ProductFrame_Main.tsx
+++ app/Component/Product_Intro/Product/ProductFrame_Main.tsx
@@ -1,11 +1,12 @@
 import { FaStar } from 'react-icons/fa';
+import Foto from "@/public/img/Product_Main/foto.png";
 
 const ProductCard = (props: any) => {
   return (
     <div className="flex flex-col border rounded-lg overflow-hidden w-60 transform transition-transform duration-500 hover:scale-110 hover:shadow-lg">      
         {/* Product Image */}
         <div className="w-full flex justify-center items-center">
-          <img src={props.product.image.src} alt={props.product.name} className="object-cover w-full " />
+          <img src={Foto.src} alt={props.product.name} className="object-cover w-full " />
         </div>
 
         {/* Product Info */}
@@ -15,14 +16,19 @@ const ProductCard = (props: any) => {
 
           {/* Brand and Rating */}
           <div className="flex justify-between items-center mt-2">
-            <span className="font-extralight text-sm underline italic">{props.product.brand}</span>
+            {/* <span className="font-extralight text-sm underline italic">{props.product.brand}</span> */}
+            {/* TEMPLATE FOR THE BRAND */}
+            <span className="font-extralight text-sm underline italic">Template</span>
+            {/* Rating */}
             <div className="flex">
               {[...Array(5)].map((star, i) => {
                 const ratingValue = i + 1;
                 return (
                   <FaStar
                     key={i}
-                    color={ratingValue <= props.product.rating ? 'yellow' : 'gray'}
+                    // color={ratingValue <= props.product.rating ? 'yellow' : 'gray'}
+                    //TEMPLATE FOR THE RATING VALUE
+                    color={ratingValue <= 5 ? 'yellow' : 'gray'}
                     size={20}
                   />
                 );
@@ -33,7 +39,8 @@ const ProductCard = (props: any) => {
           {/* Price */}
           <div className="mt-2 flex justify-between">
             <span className="text-lg font-semibold">${props.product.price}</span>
-            <span className="text-lg font-semibold text-red-500 line-through ml-2">${props.product.prSalePrice}</span>
+            {/* <span className="text-lg font-semibold text-red-500 line-through ml-2">${props.product.prSalePrice}</span> */}
+            <span className="text-lg font-semibold text-red-500 line-through ml-2">TRỜI ƠI CÚ TUI THIẾU THÔNG TIN</span>
           </div>
         </div>
     </div>
