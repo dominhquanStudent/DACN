2319dbd227a33c00a8c875e269f60a17c529586f
Do Minh Quan <quan.dobestsona@hcmut.edu.vn>
2024-10-22 19:53:05 +0700

    Update axios.js

--- api/axios.js
+++ api/axios.js
@@ -3,7 +3,8 @@ import { getCookie } from 'cookies-next';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  //baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL:petcare-be-lilac.vercel.app,
   withCredentials: true, // Ensure all requests include credentials
 });
 
